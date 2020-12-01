d2a4a2a04fc88325bde468ca5e1fa78d4bccd8fd
linzhefeng <156841569@qq.com>
2020-12-01 18:54:48 +0800

    首次加载页面获取文章数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,13 +28,18 @@ export default {
         async fetchCategories() {
             const res = await this.$http.get('/category')
             this.categories = res.data
-            // 在分类的每个对象中新增list属性
+            this.handleCategory()
+        },
+        // 在分类的每个对象中新增list属性
+        handleCategory() {
             this.categories.map(item => {
                 item.list = []
                 item.page = 0
                 item.pagesize = 10
             })
         },
+        // 获取文章数据
+        // 只获取数据
         async fetchArticle() {
             let item = this.categories[this.curIndex]
             const res = await this.$http.get('/detail/' + item._id, {
@@ -46,15 +51,22 @@ export default {
             return res
         },
         // 子组件active改变后出发的处理事件,接收tab的当前下标
+        // 处理事件
         async aChangeHandle(index) {
             // 通过这个下标来获取_id
             this.curIndex = index
             const res = await this.fetchArticle()
-            console.log(res)
+            this.categories[index].list = res.data
+        },
+        // 第一次加载页面 自动加载文章
+        async articleInit() {
+            const res = await this.fetchArticle()
+            this.categories[this.curIndex].list = res.data
         }
     },
-    created() {
-        this.fetchCategories()
+    async created() {
+        await this.fetchCategories()
+        this.articleInit()
     }
 }
 </script>
