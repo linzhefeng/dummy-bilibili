482080b813f7ff2dac5120ce7a84d290f4de67af
linzhefeng <156841569@qq.com>
2020-12-02 01:11:25 +0800

    加入文章item文件,还未完善

---
+++ src/components/home/ArticleItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p v-for="item in data.list" :key="item.id">
+            {{ item.name }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    props: ['data'],
+    methods: {}
+}
+</script>
+<style></style>
